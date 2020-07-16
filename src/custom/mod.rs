41a128742f7bcbea843ba8475d2c466954364c04
jugeeya <jugeeya@live.com>
2020-07-15 18:08:25 -0700

    initial acmd commit

---
+++ src/custom/mod.rs
@@ -0,0 +1,31 @@
+use smash::lua2cpp::{L2CFighterCommon, L2CFighterBase};
+use acmd;
+
+// Use this for general per-frame fighter-level hooks
+pub fn once_per_fighter_frame(fighter : &mut L2CFighterCommon) {
+    unsafe {
+        let module_accessor = smash::app::sv_system::battle_object_module_accessor(fighter.lua_state_agent);
+        let frame = smash::app::lua_bind::MotionModule::frame(module_accessor) as i32;
+
+        if frame % 60 == 0 {
+            println!("[Fighter Hook] Frame : {}", frame);
+        }
+    }
+}
+
+// Use this for general per-frame weapon-level hooks
+pub fn once_per_weapon_frame(fighter_base : &mut L2CFighterBase) {
+    unsafe {
+        let module_accessor = smash::app::sv_system::battle_object_module_accessor(fighter_base.lua_state_agent);
+        let frame = smash::app::lua_bind::MotionModule::frame(module_accessor) as i32;
+
+        if frame % 10 == 0 {
+            println!("[Weapon Hook] Frame : {}", frame);
+        }
+    }
+}
+
+pub fn install() {
+    acmd::add_custom_hooks!(once_per_fighter_frame);
+    acmd::add_custom_weapon_hooks!(once_per_weapon_frame);
+}
---
+++ src/mario/mod.rs
@@ -0,0 +1,37 @@
+use smash::hash40;
+use smash::lib::lua_const::*;
+use smash::lua2cpp::L2CFighterCommon;
+use acmd::{acmd, acmd_func};
+
+#[acmd_func(
+    battle_object_category = BATTLE_OBJECT_CATEGORY_FIGHTER, 
+    battle_object_kind = FIGHTER_KIND_MARIO, 
+    animation = "attack_air_f",
+    animcmd = "game_attackairf")]
+pub fn mario_fair(fighter: &mut L2CFighterCommon) {
+    acmd!({
+        frame(16)
+        AttackModule::clear_all() // clear all previous hitboxes
+        if(is_execute) {
+            ATTACK(
+                ID=0, Part=0, Bone=hash40("arml"), 19.0, 361, 80, 0, 30, 113.0, 3.2, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 1.0,
+                ATTACK_SETOFF_KIND_ON, ATTACK_LR_CHECK_F, ATTACK_LR_CHECK_F, ATTACK_LR_CHECK_F,
+                ATTACK_LR_CHECK_F, ATTACK_LR_CHECK_F, false, false, false, false, true,
+                COLLISION_SITUATION_MASK_GA, COLLISION_CATEGORY_MASK_ALL, COLLISION_PART_MASK_ALL,
+                false, hash40("collision_attr_fire"), ATTACK_SOUND_LEVEL_M, COLLISION_SOUND_ATTR_PUNCH,
+                ATTACK_REGION_PUNCH
+            )
+            rust {
+                println!("Fair frame 16");
+            }
+        }
+        sv_kinetic_energy::add_speed(1.0);
+
+    });
+}
+
+pub fn install() {
+    acmd::add_hooks!(
+        mario_fair,
+    );//mario_dtilt);
+}

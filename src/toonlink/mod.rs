827703bd39c78577085afb6f5e0e9058824d2ecb
Shieldlesscap1 <63805100+Shieldlesscap1@users.noreply.github.com>
2020-07-18 13:15:14 -0400

    Added files for every character
    - Added modules for every character (Including Giga Bowser, Ice Climbers are under one file as popo_nana, PT's Pokemon are under one file as ptrainer)
    - Added imports for L2CFighterCommon and L2CFighterBase for all characters
    - Allow unused imports

---
+++ src/toonlink/mod.rs
@@ -0,0 +1,9 @@
+use smash::hash40;
+use smash::lib::lua_const::*;
+use smash::lua2cpp::{L2CFighterCommon, L2CFighterBase};
+use acmd::{acmd, acmd_func};
+
+pub fn install() {
+    acmd::add_hooks!(
+    );
+}

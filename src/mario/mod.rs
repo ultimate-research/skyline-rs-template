eb927989096e85825831df265a7a386bc4953738
WuBoy <alexwuief@gmail.com>
2021-06-21 21:07:21 -0400

    added a fighter-specific once-per fighter frame

--- src/mario/mod.rs
+++ src/mario/mod.rs
@@ -1,7 +1,19 @@
 use smash::hash40;
+use smash::phx::Hash40;
 use smash::lib::lua_const::*;
-use smash::lua2cpp::L2CAgentBase;
+use smash::app::*;
+use smash::app::lua_bind::*;
+use smash::lua2cpp::{L2CFighterCommon, L2CAgentBase};
 use smashline::*;
+use smash_script::*;
+
+// A Once-Per-Fighter-Frame that only applies to Mario. Neat!
+#[fighter_frame( agent = FIGHTER_KIND_MARIO )]
+fn mario_frame(fighter: &mut L2CFighterCommon) {
+    unsafe {
+        println!("It'sa me, Mario, wahoooooooo!");
+    }
+}
 
 #[acmd_script( agent = "mario", script = "game_attackairf", category = ACMD_GAME )]
 unsafe fn mario_fair(fighter: &mut L2CAgentBase) {
@@ -25,8 +37,9 @@ unsafe fn mario_fair(fighter: &mut L2CAgentBase) {
     });
 }
 
-#[acmd_script( agent = "mario", script = "game_attackairlw", category = ACMD_GAME )]
-unsafe fn mario_dair(fighter: &mut L2CAgentBase) {
+// Uses the acmd! macro, if you're porting directly from Rubendal's data viewer.
+#[acmd_script( agent = "mario", script = "game_attackairlw", category = ACMD_GAME, low_priority )]
+unsafe fn mario_dair_acmd(fighter: &mut L2CAgentBase) {
     let lua_state = fighter.lua_state_agent;
     acmd!(lua_state, {
         frame(Frame=5)
@@ -59,9 +72,42 @@ unsafe fn mario_dair(fighter: &mut L2CAgentBase) {
     });
 }
 
+// Uses smash_script, if you prefer to use the built-in macros instead.
+#[acmd_script( agent = "mario", script = "game_attackairlw", category = ACMD_GAME )]
+unsafe fn mario_dair_smash_script(fighter: &mut L2CAgentBase) {
+    sv_animcmd::frame(fighter.lua_state_agent, 5.0);
+    if macros::is_excute(fighter) {
+        WorkModule::on_flag(fighter.module_accessor, *FIGHTER_STATUS_ATTACK_AIR_FLAG_ENABLE_LANDING);
+    }
+    for _ in 0..5 {
+        if macros::is_excute(fighter) {
+            macros::ATTACK(fighter, 0, 0, Hash40::new("top"), 1.4, 94, 15, 0, 50, 4.0, 0.0, -0.5, 0.0, None, None, None, 1.0, 0.8, *ATTACK_SETOFF_KIND_ON, *ATTACK_LR_CHECK_POS, true, 0, 0.0, 0, false, false, false, false, true, *COLLISION_SITUATION_MASK_GA, *COLLISION_CATEGORY_MASK_ALL, *COLLISION_PART_MASK_ALL, false, Hash40::new("collision_attr_rush"), *ATTACK_SOUND_LEVEL_S, *COLLISION_SOUND_ATTR_KICK, *ATTACK_REGION_BODY);
+            macros::ATTACK(fighter, 1, 0, Hash40::new("top"), 1.4, 94, 15, 0, 25, 7.0, 0.0, 6.0, 0.0, None, None, None, 1.0, 0.8, *ATTACK_SETOFF_KIND_ON, *ATTACK_LR_CHECK_POS, true, 0, 0.0, 0, false, false, false, false, true, *COLLISION_SITUATION_MASK_GA, *COLLISION_CATEGORY_MASK_ALL, *COLLISION_PART_MASK_ALL, false, Hash40::new("collision_attr_rush"), *ATTACK_SOUND_LEVEL_S, *COLLISION_SOUND_ATTR_KICK, *ATTACK_REGION_BODY);
+        }
+        sv_animcmd::wait(fighter.lua_state_agent, 1.0);
+        if macros::is_excute(fighter) {
+            AttackModule::clear_all(fighter.module_accessor);
+        }
+        sv_animcmd::wait(fighter.lua_state_agent, 1.0);
+    }
+    sv_animcmd::frame(fighter.lua_state_agent, 23.0);
+    if macros::is_excute(fighter) {
+        macros::ATTACK(fighter, 0, 0, Hash40::new("top"), 5.5, 75, 100, 0, 80, 11.0, 0.0, 6.8, 0.0, None, None, None, 1.0, 0.8, *ATTACK_SETOFF_KIND_ON, *ATTACK_LR_CHECK_POS, true, 0, 0.0, 0, false, false, false, false, true, *COLLISION_SITUATION_MASK_GA, *COLLISION_CATEGORY_MASK_ALL, *COLLISION_PART_MASK_ALL, false, Hash40::new("collision_attr_normal"), *ATTACK_SOUND_LEVEL_L, *COLLISION_SOUND_ATTR_KICK, *ATTACK_REGION_BODY);
+    }
+    sv_animcmd::wait(fighter.lua_state_agent, 1.0);
+    if macros::is_excute(fighter) {
+        AttackModule::clear_all(fighter.module_accessor);
+    }
+    sv_animcmd::frame(fighter.lua_state_agent, 33.0);
+    if macros::is_excute(fighter) {
+        WorkModule::off_flag(fighter.module_accessor, *FIGHTER_STATUS_ATTACK_AIR_FLAG_ENABLE_LANDING);
+    }
+}
+
 pub fn install() {
     smashline::install_acmd_scripts!(
         mario_fair,
-        mario_dair
+        mario_dair_acmd,
+        mario_dair_smash_script
     );
 }

69a43031b3002f342fe15052908bf51e2a85f721
WuBoytH <alexwuief@gmail.com>
2023-01-19 11:39:52 -0500

    Updated template

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,11 @@
-#![feature(concat_idents)]
-#![feature(proc_macro_hygiene)]
+#![feature(
+    concat_idents,
+    proc_macro_hygiene
+)]
+#![allow(
+    unused_macros,
+    clippy::borrow_interior_mutable_const
+)]
 
 mod mario;
 mod falco;
